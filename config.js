ef696f7fdcb3835a8aa286b74a8323f6239ac516
Adi Chirilov <developer@rosoftware.ro>
2019-07-17 12:46:54 +0300

    Commit

---
+++ config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteTitle: 'Site prezentare | WebsiteMarket', // <title>
+  manifestName: 'Highlights',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Portofoliu',
+  subHeading: 'Site Prezentare',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/creare-site/site-portofoliu',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://site-prezentare-afacere.websitemarket.ro/',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/Websitemarketro',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:hello@websitemarket.ro',
+    },
+  ],
+};
---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import config from '../../config';
+
+export default function Footer() {
+  return (
+    <footer>
+      <ul className="icons">
+        {config.socialLinks.map(social => {
+          const { icon, name, url } = social;
+          return (
+            <li key={url}>
+              <a href={url} className={`icon alt ${icon}`}>
+                <span className="label">{name}</span>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="copyright">
+        <li>&copy; WebsiteMarket</li>
+        <li>
+          Demo Images: <a href="http://unsplash.com">Unsplash</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
