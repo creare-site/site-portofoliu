a5318674d7ca18322af767f5428e2b97868ad6ab
Adi Chirilov <developer@rosoftware.ro>
2019-07-18 08:53:11 +0300

    Dummy

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
     <section id="header">
       <header className="major">
         <h1>{config.heading}</h1>
-        <p>{config.subHeading}</p>
+        <p>{config.subHeading} - <a href="https://websitemarket.ro?ref=portofoliu">Comanda Site</a> </p>
       </header>
       <div className="container">
         <ul className="actions special">
